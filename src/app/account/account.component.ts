916810fc6e2b5a028d46f6fd272ded4722f879e8
vsheva <valera.sheva@gmail.com>
2023-11-28 20:35:33 -0500

    add Data Services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,31 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../account.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
   // providers: [{ provide: LoggingService, useClass: LoggingTestService}]
-  providers: [LoggingService] //we say which type of service should be used in the places where this service is injected
+  providers: [LoggingService, AccountsService]  //we say which type of service should be used in the places where this service is injected
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
 
   private loggingService
 
   //it automatically create an instance of LoggingService
-  constructor(loggingService: LoggingService) {
-    this.loggingService = loggingService
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
   }
 
 
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-
+    this.accountsService.updateStatus(this.id, status)
     this.loggingService.logStatusChange(status)
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,12 +1,13 @@
 import {Component, EventEmitter, inject, Output} from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../account.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
   //providers: [{ provide: LoggingService, useClass: LoggingService}]
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
@@ -15,18 +16,14 @@ export class NewAccountComponent {
 
   //it automatically create an instance of LoggingService
 
-  constructor(loggingService: LoggingService) {
-   this.loggingService = loggingService
+  constructor(private loggingService: LoggingService,
+              private accountsService: AccountsService) {
     //this.loggingService = inject(LoggingService); //!!!
  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
-    this.loggingService.logStatusChange(accountStatus)
+    this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus)
 
   }
 }
